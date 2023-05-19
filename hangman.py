2f8ed5c7c8652d30e2fc517b2e2f50e4223ead5a
codingMaz <m.balandari.toroghi@gmail.com>
2023-05-19 12:01:32 +0330

    start creating function hangman

--- hangman.py
+++ hangman.py
@@ -1,4 +1,5 @@
 import random
+import string
 from words import words
 
 
@@ -8,3 +9,26 @@ def get_a_valid_word(words):
         word = random.choice(words)
 
     return word.upper()
+
+
+def hangman():
+    word = get_a_valid_word(words)
+    word_letters = set(word)  # letters in the word
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()  # what the user has guessed
+
+    # getting the user input
+    user_letter = input("Guess a letter: ").upper()
+    if user_letter in alphabet - used_letters:
+        used_letters.add(user_letter)
+        if user_letter in word_letters:
+            word_letters.remove(user_letter)
+
+    elif user_letter in used_letters:
+        print("You have already used that character. Please try again.")
+
+    else:
+        print("Invalid character. Please try again.")
+
+
+hangman()
